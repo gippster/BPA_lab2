d96011e77fa242e2b852c049ff8fa22cc3f839a0
sajbu <sajbullin77@gmail.com>
2023-04-11 13:45:17 +0600

    solved problems

--- parsecourse.py
+++ parsecourse.py
@@ -8,12 +8,12 @@ def course() -> float:
     page = requests.get(url)
     print(page.status_code)
     soup = BeautifulSoup(page.text, "html.parser")
-    #div class="BNeawe iBp4i AP7Wnd">81,79 Российский рубль
+    # div class="BNeawe iBp4i AP7Wnd">81,79 Российский рубль"
     usd_course = ''
-    usd = soup.findAll('div', class_ = 'BNeawe iBp4i AP7Wnd')
-    for data in usd: #Находим курс доллара
+    usd = soup.findAll('div', class_='BNeawe iBp4i AP7Wnd')
+    for data in usd:  # Находим курс доллара
         if data.find('div'):
             usd_course = data.text
     mean_of_course = float(usd_course.replace(",", ".").split()[0])
     if usd_course != '':
-        return (mean_of_course)
+        return mean_of_course
